b360e7e44798582eb0274470af1813dc12158157
Chris <bluman325@gmail.com>
2022-04-06 14:19:51 -0500

    registered super_Type

--- supers/models.py
+++ supers/models.py
@@ -4,7 +4,7 @@ from super_types.models import Super_Type
 
 class Power(models.Model):
     name = models.CharField(max_length=255)
-# Create your models here.
+
 class Super(models.Model):
     name = models.CharField(max_length=255)
     alter_ego = models.CharField(max_length=255)
@@ -13,5 +13,3 @@ class Super(models.Model):
     powers = models.ManyToManyField(Power)
     catchphrase = models.CharField(max_length=255)
     super_type = models.ForeignKey(Super_Type, on_delete=models.CASCADE)
-
-
