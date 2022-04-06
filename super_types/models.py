418a8eb304042f0ada53b09c671606f8da05c9af
Chris <bluman325@gmail.com>
2022-04-06 13:01:31 -0500

    remade migrations and refractored supers class in models

--- super_types/models.py
+++ super_types/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
+class Super_Type(models.Model):
+    type = models.CharField(max_length=255)
+
 # Create your models here.
--- supers/models.py
+++ supers/models.py
@@ -1,11 +1,17 @@
 from django.db import models
-from super_types.models import SuperType
+from super_types.models import Super_Type
 
+
+class Power(models.Model):
+    name = models.CharField(max_length=255)
 # Create your models here.
 class Super(models.Model):
     name = models.CharField(max_length=255)
     alter_ego = models.CharField(max_length=255)
-    primary_ability = models.CharField(max_length=255)
-    secondary_ability = models.CharField(max_length=255)
+    # primary_ability = models.CharField(max_length=255)
+    # secondary_ability = models.CharField(max_length=255)
+    powers = models.ManyToManyField(Power)
     catchphrase = models.CharField(max_length=255)
-    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
+    super_type = models.ForeignKey(Super_Type, on_delete=models.CASCADE)
+
+
